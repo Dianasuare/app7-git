4be2f21b26f14c326e0dcab1bf213cf66b247e6a
Dianasuare <diana992172@gmail.com>
2017-05-17 07:51:28 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,28 +1,27 @@
 // JavaScript Document
 
 $(document).ready(function(e) {
-    document.addEventListener("deviceready",function()  {
+    document.addEventListener("deviceready",function(){
 		
-		$('$izquierda').on("swipeleft",function()  {
-			navigator.notification..alert("Deslizo a la izquierda",function(){"Aplicacion7","Aceptar" });                                     
- });
- 
- $('derecha').on("swiperight",function(){
-	 navigator.notification.confirm("¿Que quieres hacer?",function(opt){
-		 switch(opt)
-		 {
-			 case 1:
-			 navigator.notification.beep(1);
-			 break;
-			 
-			 case 2:
-			 navigator.notification.vibrate(1000);
-			 break;
-		 }
-	 },"Aplicacion7","Beep,Vibrar,Cancelar");
-	 
-	 });
-	 },false);
-	 });
-	 
+		$('#izquierda').on("swipeleft",function(){
+			navigator.notification.alert("Deslizo a la Izquierda",function(){"Aplicacion7","Aceptar"});
+});
+
+$('#derecha').on("swiperight",function(){
+	navigator.notification.confirm("¿Que quieres saber?",function(opt){
+		switch(opt)
+		{
+			case 1:
+			navigator.notification.beep(1);
+			break;
+			
+			case 2:
+			navigator.notification.vibrate(1000);
+			break;
+		}
+	},"Aplicacio7","Beep,Vibrar,Cancelar");
+	
+});
+	},false);
+});
 	 
